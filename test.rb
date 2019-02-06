c4e2e266d9c7605f64cc75dffb280dfb91fa6bf2
Trevor Hess <trevor.ghess@gmail.com>
2019-02-05 16:47:14 -0800

    adding IIS tests

--- test.rb
+++ test.rb
@@ -1,3 +1,14 @@
 describe os.family do
   it { should eq 'windows' }
 end
+
+# Check that IIS is installed via Windows Features
+describe windows_feature('Webserver') do
+  it { should be_installed }
+end
+
+# Ensure IIS Service is running
+describe service('W3SVC') do
+  it { should be_installed }
+  it { should be_running }
+end
